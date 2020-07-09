51348ab3ed828f473601307fb3cbac91ff5b70b8
JingruiZhang <ruilovechu@live.com>
2020-07-09 20:06:13 +0800

    添加 实例方法 SendMsg2

--- sln.JingruiZhang.Util/JingruiZhang.Util.FleckNet45ClientTest/Program.cs
+++ sln.JingruiZhang.Util/JingruiZhang.Util.FleckNet45ClientTest/Program.cs
@@ -1,19 +1,66 @@
-﻿using System;
+﻿using SuperSocket.ClientEngine;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using WebSocket4Net;
 
 namespace JingruiZhang.Util.FleckNet45ClientTest
 {
     class Program
     {
+        #region 一次性发送 WebSocket 消息给服务器端
+        WebSocket4Net.WebSocket websocket;
+        string data;
+
+        /// <summary>
+        /// 连接 url，向服务器端发消息
+        /// </summary>
+        /// <param name="wsOrWssUrl">ws或wss地址</param>
+        /// <param name="data">要发送的字符串</param>
+        public void SendMsg2(string wsOrWssUrl, string data)
+        {
+            this.data = data;
+            websocket = new WebSocket4Net.WebSocket(wsOrWssUrl);
+            websocket.Opened += websocket_Opened;
+            websocket.Closed += websocket_Closed;
+            websocket.Error += Websocket_Error;
+            websocket.MessageReceived += websocket_MessageReceived;
+            websocket.Open();
+        }
+
+        private void websocket_MessageReceived(object sender, MessageReceivedEventArgs e)
+        {
+        }
+
+        private void Websocket_Error(object sender, ErrorEventArgs e)
+        {
+        }
+
+        private void websocket_Closed(object sender, EventArgs e)
+        {
+        }
+
+        private void websocket_Opened(object sender, EventArgs e)
+        {
+            websocket.Send(this.data);
+            websocket.Close();
+        }
+        #endregion
+
         static void Main(string[] args)
         {
-            var clientObj = new Fleck.WebSocketConnection(listenUrl);
+            string url = "wss://www.probim.cn:8083";
+            string data = @"{""Title"":""【系统通知】"",""Content"":""这是一条系统通知"",""Receiver"":""all""}";
 
-            var ws = new ClientWebSocket();
-            await ws.ConnectAsync(new Uri("ws://127.0.0.1:8080"), CancellationToken.None);
+            Program p = new Program();
+            p.SendMsg2(url, data);
+
+            Console.ReadKey();
         }
+
+      
     }
 }
--- sln.JingruiZhang.Util/JingruiZhang.Util/ZWebSocketClientHelper.cs
+++ sln.JingruiZhang.Util/JingruiZhang.Util/ZWebSocketClientHelper.cs
@@ -5,6 +5,8 @@ using System.Net.WebSockets;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using SuperSocket.ClientEngine;
+using WebSocket4Net;
 
 namespace JingruiZhang.Util
 {
@@ -13,11 +15,14 @@ namespace JingruiZhang.Util
     /// </summary>
     public class ZWebSocketClientHelper
     {
+      
+
         /// <summary>
         /// 连接url，发送广播消息，并断开
         /// </summary>
         /// <param name="wsOrWssUrl"></param>
         /// <param name="data"></param>
+        [Obsolete("此方法不稳定，不建议用")]
         public static async void SendMsg(string wsOrWssUrl, string data)
         {
             try
@@ -36,5 +41,44 @@ namespace JingruiZhang.Util
             }
 
         }
+
+        #region 一次性发送 WebSocket 消息给服务器端
+        WebSocket4Net.WebSocket websocket;
+        string data;
+
+        /// <summary>
+        /// 连接 url，向服务器端发消息
+        /// </summary>
+        /// <param name="wsOrWssUrl">ws或wss地址</param>
+        /// <param name="data">要发送的字符串</param>
+        public void SendMsg2(string wsOrWssUrl, string data)
+        {
+            this.data = data;
+            websocket = new WebSocket4Net.WebSocket(wsOrWssUrl);
+            websocket.Opened += websocket_Opened;
+            websocket.Closed += websocket_Closed;
+            websocket.Error += Websocket_Error;
+            websocket.MessageReceived += websocket_MessageReceived;
+            websocket.Open();
+        }
+
+        private void websocket_MessageReceived(object sender, MessageReceivedEventArgs e)
+        {
+        }
+
+        private void Websocket_Error(object sender, ErrorEventArgs e)
+        {
+        }
+
+        private void websocket_Closed(object sender, EventArgs e)
+        {
+        }
+
+        private void websocket_Opened(object sender, EventArgs e)
+        {
+            websocket.Send(this.data);
+            websocket.Close();
+        }
+        #endregion
     }
 }
